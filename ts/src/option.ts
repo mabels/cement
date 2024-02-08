5ac7f08e4bd457d76a646cf5c80452dba0d1b2d9
Meno Abels <meno.abels@adviser.com>
2024-02-08 10:09:47 +0100

    chore: added option and imported result

---
+++ ts/src/option.ts
@@ -0,0 +1,60 @@
+
+export abstract class Option<T> {
+    static Some<T>(t: T): Option<T> {
+        return new Some(t);
+    }
+
+    static None<T>(): Option<T> {
+        return new None();
+    }
+
+    static Is<T>(t: unknown): t is Option<T> {
+        return t instanceof Option;
+    }
+
+    IsNone(): boolean {
+        return this.is_none();
+    }
+
+    IsSome(): boolean {
+        return this.is_some();
+    }
+    Unwrap(): T {
+        return this.unwrap();
+    }
+
+
+    abstract is_none(): boolean;
+    abstract is_some(): boolean;
+    abstract unwrap(): T;
+}
+
+export class Some<T> extends Option<T>{
+    private _t: T;
+    constructor(_t: T) {
+        super();
+        this._t = _t;
+    }
+
+    is_none(): boolean {
+        return false
+    }
+    is_some(): boolean {
+        return true
+    }
+    unwrap(): T {
+        return this._t;
+    }
+}
+
+export class None<T> extends Option<T>{
+    is_none(): boolean {
+        return true
+    }
+    is_some(): boolean {
+        return false
+    }
+    unwrap(): T {
+        throw new Error("None.unwrap");
+    }
+}
