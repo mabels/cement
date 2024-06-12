4b87b28bfd6d070b3074687a48fb4081f555f700
Meno Abels <meno.abels@adviser.com>
2024-06-12 18:19:29 +0200

    chore: cjs and esm imports

--- ts/smoke/smoke.ts
+++ ts/smoke/smoke.ts
@@ -1,6 +1,6 @@
-import { WebSysAbstraction } from "@adviser/cement/web";
-import { NodeSysAbstraction } from "@adviser/cement/node";
-import { LoggerImpl, Result, Option } from "@adviser/cement";
+import { WebSysAbstraction } from "@adviser/cement/web"
+import { NodeSysAbstraction } from "@adviser/cement/node"
+import { LoggerImpl, Result, Option, Level } from "@adviser/cement"
 
 (async () => {
   const none = Option.None();
@@ -8,8 +8,9 @@ import { LoggerImpl, Result, Option } from "@adviser/cement";
   if (!result.isOk()) {
     throw new Error("Result is Err");
   }
-  const log = new LoggerImpl();
-
+  const log = new LoggerImpl().EnableLevel(Level.DEBUG).With()
+      .Str("runtime", globalThis.Deno ? "Deno" : "Node")
+      .Str("version", process?.version || globalThis.Deno.version.deno).Logger();
   {
     const sys = NodeSysAbstraction();
     log.Info().Str("id", sys.NextId()).Msg("Node-Alright");
