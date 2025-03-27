0c7b376963b2994c3fc75b5618304be52e163865
Meno Abels <meno.abels@adviser.com>
2025-03-27 09:43:25 +0100

    fix: path-ops to be more compatible with node

--- ts/src/path-ops.test.ts
+++ ts/src/path-ops.test.ts
@@ -5,21 +5,37 @@ it("basename", () => {
   expect(pathOps.basename("a/b/c/")).toBe("c");
   expect(pathOps.basename("c")).toBe("c");
   expect(pathOps.basename("c/")).toBe("c");
+  expect(pathOps.basename("/a/b/c")).toBe("c");
+  expect(pathOps.basename("/a/b/c/")).toBe("c");
+  expect(pathOps.basename("/c")).toBe("c");
+  expect(pathOps.basename("/c/")).toBe("c");
   expect(pathOps.basename("")).toBe("");
 });
 
 it("dirname", () => {
   expect(pathOps.dirname("a/b/c")).toBe("a/b");
+  expect(pathOps.dirname("/a/b/c")).toBe("/a/b");
   expect(pathOps.dirname("a/b/c/")).toBe("a/b");
-  expect(pathOps.dirname("c")).toBe("");
-  expect(pathOps.dirname("c/")).toBe("");
-  expect(pathOps.dirname("")).toBe("");
+  expect(pathOps.dirname("/a/b/c/")).toBe("/a/b");
+  expect(pathOps.dirname("///a///b/./c/")).toBe("/a/b");
+  expect(pathOps.dirname("c")).toBe(".");
+  expect(pathOps.dirname("./c/d")).toBe("c");
+  expect(pathOps.dirname("c/")).toBe(".");
+  expect(pathOps.dirname("")).toBe(".");
+  expect(pathOps.dirname("./yyy/./a")).toBe("yyy");
 });
 
 it("join", () => {
   expect(pathOps.join("a", "b", "c")).toBe("a/b/c");
   expect(pathOps.join("a", "b", "c/")).toBe("a/b/c");
+  expect(pathOps.join("", "a", "", "b", "", "c/")).toBe("a/b/c");
   expect(pathOps.join("c")).toBe("c");
   expect(pathOps.join("c/")).toBe("c");
-  expect(pathOps.join("")).toBe("");
+  expect(pathOps.join("")).toBe(".");
+  expect(pathOps.join(".", "yyy")).toBe("yyy");
+  expect(pathOps.join(".", "yyy", ".", "a")).toBe("yyy/a");
+  expect(pathOps.join("./")).toBe(".");
+  expect(pathOps.join(".")).toBe(".");
+  expect(pathOps.join(".", ".")).toBe(".");
+  expect(pathOps.join(".", ".", ".", "c")).toBe("c");
 });
