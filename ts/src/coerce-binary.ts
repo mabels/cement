b4aa4e22a5e18ebdbb9dcf1389647f2b6e22e1db
Meno Abels <meno.abels@adviser.com>
2025-10-27 10:06:12 +0100

    feat: isArrayBuffer and isUint8Array

--- ts/src/coerce-binary.ts
+++ ts/src/coerce-binary.ts
@@ -2,6 +2,14 @@ import { TxtEnDecoderSingleton } from "./txt-en-decoder.js";
 
 export type CoerceBinaryInput = string | ArrayBufferLike | ArrayBufferView | Uint8Array; // | SharedArrayBuffer
 
+export function isArrayBuffer(value: unknown): value is ArrayBuffer {
+  return value instanceof ArrayBuffer || Object.prototype.toString.call(value) === "[object ArrayBuffer]";
+}
+
+export function isUint8Array(value: unknown): value is Uint8Array {
+  return value instanceof Uint8Array || Object.prototype.toString.call(value) === "[object Uint8Array]";
+}
+
 export async function top_uint8(input: CoerceBinaryInput | Blob): Promise<Uint8Array> {
   if (input instanceof Blob) {
     return new Uint8Array(await input.arrayBuffer());
@@ -13,11 +21,11 @@ export function to_uint8(input: CoerceBinaryInput, encoder?: TextEncoder): Uint8
   if (typeof input === "string") {
     return (encoder ?? TxtEnDecoderSingleton()).encode(input);
   }
-  if (input instanceof ArrayBuffer /* || input instanceof SharedArrayBuffer */) {
+  if (isArrayBuffer(input)) {
     return new Uint8Array(input);
   }
 
-  if (input instanceof Uint8Array) {
+  if (isUint8Array(input)) {
     return input;
   }
   // not nice but we make the cloudflare types happy
